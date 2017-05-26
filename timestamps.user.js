5426628f87417de8b04fe05bd782673de52e6a1e
Justin Ian Spencer <justinspencer@tfwno.gf>
2017-05-26 05:02:14 +0200

    Create timestamps.user.js

---
+++ timestamps.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Vola Timestamps
+// @namespace    https://volafile.org/
+// @version      2
+// @description  Dongo said to make this
+// @author       Justin
+// @match        https://volafile.org/r/*
+// @grant        none
+// @require      https://cdn.rawgit.com/spencinator/scripts/232620aaad8abff7d49258f85a495f652d80f3ca/dry.js
+// @run-at       document-start
+// ==/UserScript==
+
+dry.once("dom", () => {
+    "use strict";
+    console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
+
+    const style = document.createElement("style");
+    style.textContent = `
+.username.timestamp {
+  -moz-user-select: none;
+  user-select: none;
+  display: inline-block;
+}
+[timestamps="false"] .username.timestamp {
+  display: none;
+}
+`;
+    document.body.appendChild(style);
+
+    const config_key = `${dry.config.room_id}-timestamps`;
+    let enabled = localStorage.getItem(config_key);
+    enabled = enabled !== "disabled";
+
+    new class extends dry.MessageFilter {
+        showMessage(orig, nick, message, options) {
+            if (!options.timestamp) {
+                options.timestamp = Date.now();
+            }
+        }
+        addMessage(orig, m) {
+            if (!m.nick || !m.options || !m.options.timestamp) {
+                return;
+            }
+            let span = document.createElement("span");
+            span.classList.add("timestamp", "username");
+            let d = new Date(m.options.timestamp);
+            span.textContent = d.toLocaleString("en-US", {
+                hour12: false,
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            }) + " |";
+            span.setAttribute("title", d.toLocaleString("eu"));
+            m.timestamp_elem = span;
+            m.elem.insertBefore(span, m.elem.firstChild);
+        }
+    }();
+    new class extends dry.Commands {
+        ts(e) {
+            enabled = !enabled;
+            localStorage.setItem(config_key, enabled ? "enabled" : "disabled");
+            document.body.setAttribute("timestamps", "" + enabled);
+            return true;
+        }
+    }();
+});
