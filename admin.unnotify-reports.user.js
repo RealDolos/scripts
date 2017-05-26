1ec26887c269cf641acddfcc94e0edf0a7459b8a
Justin Ian Spencer <justinspencer@tfwno.gf>
2017-05-26 05:12:20 +0200

    Create admin.unnotify-reports.user.js

---
+++ admin.unnotify-reports.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Unnotify reports
+// @namespace    http://volafile.org
+// @version      1
+// @description  because it's annoying
+// @author       Me
+// @match        https://volafile.org/r/*
+// @grant        none
+// @require      https://cdn.rawgit.com/spencinator/scripts/232620aaad8abff7d49258f85a495f652d80f3ca/dry.js
+// @run-at       document-start
+// ==/UserScript==
+
+dry.once("dom", () => {
+    "use strict";
+    console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
+    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @|periscopefollower/;
+
+    new class extends dry.MessageFilter {
+        showMessage(orig, nick, message, options) {
+            if (nick === "Report" && options.staff) {
+                options.notify = options.highlight = false;
+                if (message[0] && message[0].value && message[0].value.includes("Enabled notifications in this room")) {
+                    return false;
+                }
+                if (message[1] && message[1].value) {
+                    let text = message[1].value;
+                    if (text.includes("BLACKLIST") && BLACK.test(text)) {
+                        console.error(message);
+                        return false;
+                    }
+                }
+                return;
+            }
+            if (nick === "Log" && options.staff) {
+                options.notify = options.highlight = false;
+            }
+        }
+    }();
+});
