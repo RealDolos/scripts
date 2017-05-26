609ad0388c7be256b506f6d5a8923c50eb2f6126
Justin Ian Spencer <justinspencer@tfwno.gf>
2017-05-26 05:04:01 +0200

    Create gallery-is-not-for-chat.user.js

---
+++ gallery-is-not-for-chat.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         gallery is not for chat
+// @namespace    https://volafile.org/
+// @version      0.3
+// @description  It really is not!
+// @match        https://volafile.org/r/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    const style = document.createElement("style");
+    style.textContent = `
+.blur {
+    -webkit-filter: none;
+    -moz-filter:  none;
+    -o-filter: none;
+    -ms-filter: none;
+    filter: none;
+}
+.blur #call_to_action_container,
+.blur #files_scroller,
+.blur #radio_container {
+    -webkit-filter: blur(5px);
+    -moz-filter: blur(5px);
+    -o-filter: blur(5px);
+    -ms-filter: blur(5px);
+    filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius='5');
+}
+`;
+    document.body.appendChild(style);
+
+    const frame = document.querySelector("#files_frame");
+    frame.appendChild(document.querySelector("#gallery_frame"));
+})();
